237e9d03ecd25ee83b31f18c96491d243e0594a2
Ojas Tripathi <ojastrips@gmail.com>
2023-10-06 16:10:51 +0530

    rename columns and splice together dataframes

--- splitter.py
+++ splitter.py
@@ -7,9 +7,13 @@ def split(raw_frame: pd.DataFrame):
     print(raw_frame.columns[2])
     year = str(raw_frame.columns[2])
     if "2022" in year:
-        columns = [0, 2]
+        column_name = raw_frame.columns[2]
+        columns = [2]
         output = raw_frame.iloc[:, columns]
-        print(output)
+        indexes = raw_frame.iloc[:, [0]]
+        output.rename(columns={column_name: "MMM"}, inplace=True)
+        indexes[output.columns[0]] = output.iloc[:, [0]]
+        print(indexes)
 
 
 if __name__ == "__main__":
