6052529028bbda36eba0d50916cde5790f595cbf
Ojas Tripathi <ojastrips@gmail.com>
2023-10-11 11:45:00 +0530

    make empty fields appear as missing

--- prettify.py
+++ prettify.py
@@ -30,11 +30,22 @@ cashFields = ["Operating Cash Flow", "Free Cash Flow"]
 # Extractor functions for each field
 def prettifyIncome(income_df: pd.DataFrame):
     df_output = pd.DataFrame()
+    indexes = list(income_df.columns)
+    print(indexes)
     for i in range(len(incomeFields)):
         try:
             temp = income_df.loc[incomeFields[i]].copy()
+            print(temp)
             df_output = pd.concat([df_output, temp], axis=1)
         except KeyError:
+            data = []
+            for i in range(len(indexes)):
+                data.append("missing")
+
+            print(data)
+            temp = pd.Series(data, index=indexes, name=incomeFields[i])
+            print(temp)
+            df_output = pd.concat([df_output, temp], axis=1)
             continue
     return df_output
 
@@ -46,7 +57,7 @@ def prettifyBalance(balance_df: pd.DataFrame):
             temp = balance_df.loc[balanceFields[i]].copy()
             df_output = pd.concat([df_output, temp], axis=1)
         except KeyError:
-            continue
+            df_output
     return df_output
 
 
@@ -62,7 +73,8 @@ def prettifyCash(cash_df: pd.DataFrame):
 
 
 def main():
-    print("hello")
+    masterframe = pd.read_excel("Data.xlsx", sheet_name="Master")
+    print(prettifyIncome(masterframe))
 
 
 if __name__ == "__main__":
--- splitter.py
+++ splitter.py
@@ -10,12 +10,21 @@ sheets = ["2022", "2021", "2020", "2019"]
 def sheet_exists(year, sheets: list[str]):
     output = False
     for years in sheets:
-        if year in sheets:
+        if year in years:
             output = True
             return output
         else:
             continue
     return output
+
+
+def year_splitter(year: str, series: pd.Series):
+    sheetname = year
+    temp = series.iloc[0]
+    print(temp)
+    if year in str(temp):
+        series.iloc[0] = year
+    return pd.DataFrame(series).T
 
 
 def split(raw_frame: pd.DataFrame, symbol, output_frames: list[pd.DataFrame]):
@@ -28,76 +37,16 @@ def split(raw_frame: pd.DataFrame, symbol, output_frames: list[pd.DataFrame]):
 
     sheetnames = book.sheetnames
     print(sheetnames)
-    for column in range(len(raw_frame.columns)):
-        year = str(raw_frame.columns[column])
-        #   TODO: Functionise this
-        if "2022" in year:
-            if sheet_exists("2022", sheetnames) == True:
-                columns = [column]
-            else:
-                columns = [0, column]
-            column_name = raw_frame.columns[column]
-
-            output = raw_frame.iloc[:, columns]
-            output.rename(columns={column_name: symbol}, inplace=True)
-            t2022 = pd.concat([t2022, output], axis=1)
-
-        if "2021" in year:
-            if sheet_exists("2021", sheetnames) == True:
-                columns = [column]
-            else:
-                columns = [0, column]
-
-            column_name = raw_frame.columns[column]
-            output = raw_frame.iloc[:, columns]
-            output.rename(columns={column_name: symbol}, inplace=True)
-            t2021 = pd.concat([t2021, output], axis=1)
-
-        if "2020" in year:
-            if sheet_exists("2020", sheetnames) == True:
-                columns = [column]
-            else:
-                columns = [0, column]
-
-            column_name = raw_frame.columns[column]
-            output = raw_frame.iloc[:, columns]
-            output.rename(columns={column_name: symbol}, inplace=True)
-            t2020 = pd.concat([t2020, output], axis=1)
-
-        if "2019" in year:
-            if sheet_exists("2019", sheetnames) == True:
-                columns = [column]
-            else:
-                columns = [0, column]
-
-            column_name = raw_frame.columns[column]
-            output = raw_frame.iloc[:, columns]
-            output.rename(columns={column_name: symbol}, inplace=True)
-            t2019 = pd.concat([t2019, output], axis=1)
-        else:
-            continue
-        outputs = [t2022, t2021, t2020, t2019]
-        for i in range(len(outputs)):
-            if outputs[i].empty:
-                continue
-            else:
-                ex.excel_inserter(
-                    dataframe=outputs[i],
-                    file_name="Data.xlsx",
-                    sheetname=sheets[i],
-                    append_by="column",
-                    indexing=False,
-                )
 
 
 def runner():
-    first = second = third = fourth = pd.DataFrame()
-    output_names = [first, second, third, fourth]
     masterframe = pd.read_excel("Data.xlsx", sheet_name="Master")
-    print(masterframe)
-    x = split(masterframe, "MMM", output_names)
-    return x
+    test_series = masterframe.iloc[0]
+    test_series = year_splitter("2022", test_series)
+    # split(masterframe, "MMM", output_names)
+
+    ex.excel_inserter(test_series, "output.xlsx", "2022", "row", False)
 
 
 if __name__ == "__main__":
-    runner()
+    print(runner())
