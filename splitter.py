0b4b639fa39ad8cc59ac164e02d9151360cab3af
Ojas Tripathi <ojastrips@gmail.com>
2023-10-12 08:25:39 +0530

    setup a version of splitter that accurately works with looper

--- splitter.py
+++ splitter.py
@@ -27,16 +27,76 @@ def year_splitter(year: str, series: pd.Series):
     return pd.DataFrame(series).T
 
 
-def split(raw_frame: pd.DataFrame, symbol, output_frames: list[pd.DataFrame]):
+def split(raw_frame: pd.DataFrame, symbol: str):
     #    TODO: Loop this
-    t2022 = output_frames[0]
-    t2021 = output_frames[1]
-    t2020 = output_frames[2]
-    t2019 = output_frames[3]
+    t2022 = pd.Series()
+    t2021 = pd.Series()
+    t2020 = pd.Series()
+    t2019 = pd.Series()
+
     book = openpyxl.load_workbook("Data.xlsx")
 
     sheetnames = book.sheetnames
     print(sheetnames)
+    indexes = list(raw_frame.iloc[0:, 0])
+    for row in range(len(indexes)):
+        check = str(indexes[row])
+
+        if "2022" in check:
+            print(raw_frame.iloc[row, :])
+            output = raw_frame.iloc[row, :]
+            output.iloc[0] = symbol
+            output = pd.DataFrame(output).T
+
+            ex.excel_inserter(
+                indexing=False,
+                dataframe=output,
+                file_name="output.xlsx",
+                sheetname="2022",
+                append_by="row",
+            )
+        if "2021" in check:
+            print(raw_frame.iloc[row, :])
+            output = raw_frame.iloc[row, :]
+            output.iloc[0] = symbol
+            output = pd.DataFrame(output).T
+
+            ex.excel_inserter(
+                indexing=False,
+                dataframe=output,
+                file_name="output.xlsx",
+                sheetname="2021",
+                append_by="row",
+            )
+        if "2020" in check:
+            print(raw_frame.iloc[row, :])
+            output = raw_frame.iloc[row, :]
+            output.iloc[0] = symbol
+            output = pd.DataFrame(output).T
+
+            ex.excel_inserter(
+                indexing=False,
+                dataframe=output,
+                file_name="output.xlsx",
+                sheetname="2020",
+                append_by="row",
+            )
+        if "2019" in check:
+            print(raw_frame.iloc[row, :])
+            output = raw_frame.iloc[row, :]
+            output.iloc[0] = symbol
+            output = pd.DataFrame(output).T
+
+            ex.excel_inserter(
+                indexing=False,
+                dataframe=output,
+                file_name="output.xlsx",
+                sheetname="2019",
+                append_by="row",
+            )
+        else:
+            print("Nope")
+    return
 
 
 def runner():
@@ -45,7 +105,8 @@ def runner():
     test_series = year_splitter("2022", test_series)
     # split(masterframe, "MMM", output_names)
 
-    ex.excel_inserter(test_series, "output.xlsx", "2022", "row", False)
+    # ex.excel_inserter(test_series, "output.xlsx", "2022", "row", False)
+    split(masterframe, "MMM")
 
 
 if __name__ == "__main__":
