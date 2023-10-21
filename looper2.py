c82c0aa31449a8d2698dae922b8511e1f0b34f0b
Ojas Tripathi <ojastrips@gmail.com>
2023-10-21 07:09:58 +0530

    add verbose terminal debugging

--- looper2.py
+++ looper2.py
@@ -1,6 +1,7 @@
 import looper
 import reader
 import MacroTrends as mt
+import time
 
 
 def main():
@@ -8,6 +9,8 @@ def main():
         symbol = looper.SP500_Tickers[i]
         url = f"https://www.macrotrends.net/stocks/charts/{symbol}/{reader.data2[i]}/number-of-employees"
         mt.scrape_website(url, symbol)
+        print(f"{i}/503 completed: {looper.SP500_Tickers[i]}")
+        time.sleep(3)
 
 
 if __name__ == "__main__":
